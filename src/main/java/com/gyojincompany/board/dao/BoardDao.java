992b9e1afbe15a2da9937ba46fece189940df6f1
gyojincompany <luckyred@gmail.com>
2023-06-16 12:57:29 +0900

    dao 글 리스트 반환 메서드 list() 추가

--- src/main/java/com/gyojincompany/board/dao/BoardDao.java
+++ src/main/java/com/gyojincompany/board/dao/BoardDao.java
@@ -2,10 +2,15 @@ package com.gyojincompany.board.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Timestamp;
+import java.util.ArrayList;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
+
+import com.gyojincompany.board.dto.BoardDto;
 
 public class BoardDao {
 	
@@ -56,6 +61,55 @@ public class BoardDao {
 		
 	}
 	
-	
+	public ArrayList<BoardDto> list() {
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		ArrayList<BoardDto> boardDtos = new ArrayList<BoardDto>();
+		
+		try {
+			conn = dataSource.getConnection();
+			String sql = "SELECT * FROM mvc_board ORDER BY bid DESC";
+			
+			pstmt = conn.prepareStatement(sql);
+						
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				int bid = rs.getInt("bid");
+				String bname= rs.getString("bname");
+				String btitle= rs.getString("btitle");
+				String bcontent= rs.getString("bcontent");
+				Timestamp bdate = rs.getTimestamp("bdate");
+				int bhit = rs.getInt("bhit");
+				
+				BoardDto boardDto = new BoardDto(bid, bname, btitle, bcontent, bdate, bhit);
+				
+				boardDtos.add(boardDto);
+				
+			}
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(rs != null) {
+					rs.close();
+				}				
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				if(conn != null) {
+					conn.close();
+				}
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return boardDtos;
+	}
 
 }
